14197e7040fce590ff47cf666c661fec18829ccd
NickGhignatti <nick.ghignatti@gmail.com>
2025-03-11 10:20:31 +0100

    implement map method

--- src/test/scala/task5/OptionalTest.scala
+++ src/test/scala/task5/OptionalTest.scala
@@ -5,29 +5,30 @@ import org.junit.Assert.*
 import Optionals.*
 
 class OptionalTest:
-  @Test def emptyOptionalShouldBeEmpty(): Unit =
-    val empty = Optional.Empty()
-    assertTrue(Optional.isEmpty(empty))
+    @Test def emptyOptionalShouldBeEmpty(): Unit =
+        val empty = Optional.Empty()
+        assertTrue(Optional.isEmpty(empty))
 
-  @Test def nonEmptyOptionalShouldNotBeEmpty(): Unit =
-    val nonEmpty = Optional.Maybe(0)
-    assertFalse(Optional.isEmpty(nonEmpty))
+    @Test def nonEmptyOptionalShouldNotBeEmpty(): Unit =
+        val nonEmpty = Optional.Maybe(0)
+        assertFalse(Optional.isEmpty(nonEmpty))
 
-  @Test def orElseShouldReturnDefaultWhenEmpty(): Unit =
-    val nonEmpty = Optional.Maybe(0)
-    assertEquals(0, Optional.orElse(nonEmpty, 1))
+    @Test def orElseShouldReturnDefaultWhenEmpty(): Unit =
+        val nonEmpty = Optional.Maybe(0)
+        assertEquals(0, Optional.orElse(nonEmpty, 1))
 
-  @Test def orElseShouldReturnValueWhenNonEmpty(): Unit =
-    val empty = Optional.Empty()
-    assertEquals(1, Optional.orElse(empty, 1))
+    @Test def orElseShouldReturnValueWhenNonEmpty(): Unit =
+        val empty = Optional.Empty()
+        assertEquals(1, Optional.orElse(empty, 1))
 
-  /** Task 5 -- Look the behaviour of map operator */
-  @Test def mapShouldReturnEmptyWhenEmpty(): Unit =
-    val empty: Optional[Int] = Optional.Empty()
-    val result = Optional.map(empty, _ + 1)
-    assertTrue(Optional.isEmpty(result))
+    /** Task 5 -- Look the behaviour of map operator */
+    @Test def mapShouldReturnEmptyWhenEmpty(): Unit =
+        val empty: Optional[Int] = Optional.Empty()
+        val result = Optional.map(empty, _ + 1)
+        assertTrue(Optional.isEmpty(result))
 
-  @Test def mapShouldReturnTransformedValueWhenNonEmpty(): Unit =
-    val nonEmpty = Optional.Maybe(0)
-    val result = Optional.map(nonEmpty, _ + 1)
-    assertEquals(1, Optional.orElse(result, 1))
+    @Test def mapShouldReturnTransformedValueWhenNonEmpty(): Unit =
+        val nonEmpty = Optional.Maybe(0)
+        val result = Optional.map(nonEmpty, _ + 1)
+        assertEquals(1, Optional.orElse(result, 1))
+
